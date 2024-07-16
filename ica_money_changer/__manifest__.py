8db858aa8a21923b1b7a0d5a53023eb1689e8259
Agga <agga@Aggas-MacBook-Air.local>
2024-07-17 06:18:16 +0700

    [add] ica money changer

---
+++ ica_money_changer/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "ICA Money Changer",
+    "depends": ["base", "web"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ica_money_changer.xml",
+        "views/menus.xml",
+    ],
+    "license": "LGPL-3"
+}
